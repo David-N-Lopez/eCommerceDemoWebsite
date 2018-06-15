fb944bdde698af401bf3d7c4a108890d25920abc
David Lopez <dlopez@G000304.local>
2018-06-15 09:12:18 -0500

    Contact us, and write us verification

--- src/js/main.js
+++ src/js/main.js
@@ -12,8 +12,70 @@ Respond.to([
         }
     }
 ])
-$(".go-up").click(function() {
-        $('#masthead').animate({
+$(document).ready(function(){
+    $(".go-up").click(function() {
+        $('html, body').animate({
             scrollTop: 0
         }, 2000);
-  });
+     });
+    $(".submit-btn").click(function(){
+        var $userName = $(".name").val();
+        var $userEmail = $(".email").val();
+        var $userPhone = $(".phone").val();
+        if($userName == ''){
+            $(".name").removeClass("is-valid");
+            $(".name").addClass("is-invalid");
+            $(".name-comment > span").removeClass("success-comment");
+            $(".name-comment > span").addClass("error-comment");
+            $(".name-comment > span").text("Please include your name!");
+        }
+        if($userName != ""){
+            $(".name").removeClass("is-invalid");
+            $(".name").addClass("is-valid");
+            $(".name-comment > span").removeClass("error-comment");
+            $(".name-comment > span").addClass("success-comment");
+            $(".name-comment > span").text("Looking good!")
+
+
+        }
+        if(validateEmail($userEmail)){
+            $(".email").removeClass("is-invalid");
+            $(".email").addClass("is-valid");
+            $(".email-comment > span").removeClass("error-comment");
+            $(".email-comment > span").addClass("success-comment");
+            $(".email-comment > span").text("Looking Good!");
+        }
+        if(!(validateEmail($userEmail))){
+            $(".email").removeClass("is-valid");
+            $(".email").addClass("is-invalid");
+            $(".email-comment > span").removeClass("success-comment");
+            $(".email-comment > span").addClass("error-comment");
+            $(".email-comment > span").text("Please type in a correct email!");
+            
+        }
+
+        if(validatePhone($userPhone)){
+            $(".phone").removeClass("is-invalid");
+            $(".phone").addClass("is-valid");
+            $(".phone-comment > span").removeClass("error-comment");
+            $(".phone-comment > span").addClass("success-comment");
+            $(".phone-comment > span").text("Looking good!");
+        }
+        if(!(validatePhone($userPhone))){
+            $(".phone").removeClass("is-valid");
+            $(".phone").addClass("is-invalid");
+            $(".phone-comment > span").removeClass("success-comment");
+            $(".phone-comment > span").addClass("error-comment");
+            $(".phone-comment > span").text("Please enter a correct phone number!");
+        }
+    })
+
+})
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+function validatePhone(phone){
+    var phoneno = /^[0-9-+]+$/;
+    return (phoneno.test(phone)&&(phone.length>9));
+  }
